59053bdcbcf4f5bab6f3277be2cbff0a40a5a2ba
Debasish <debasishmisra3008@gmail.com>
2016-08-09 14:05:26 +0530

    test

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 var router = require('./routes');
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static( __dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+// app.post('/reg', function(req, res) {
+//   res.send('Registered');
+//   console.log('You sent the name "' + req.body);
+// });
 
 app.use('/',router);
 
